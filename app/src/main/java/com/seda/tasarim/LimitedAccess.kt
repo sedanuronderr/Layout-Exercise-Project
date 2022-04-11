c5eb20ce4efa6f30a3903b01f5e18d7b2440aa29
SedaNur35 <sednrondr@gmail.com>
2022-04-11 21:33:01 +0300

    Initial commit

---
+++ app/src/main/java/com/seda/tasarim/LimitedAccess.kt
@@ -0,0 +1,11 @@
+package com.seda.tasarim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LimitedAccess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_limited_access)
+    }
+}
