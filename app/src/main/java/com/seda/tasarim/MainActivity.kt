73372954c0a19e7df679d1412befc0aebd5472b0
SedaNur35 <sednrondr@gmail.com>
2022-04-26 13:40:09 +0300

    Databinding added.

--- app/src/main/java/com/seda/tasarim/MainActivity.kt
+++ app/src/main/java/com/seda/tasarim/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.seda.tasarim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.seda.tasarim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    //    setContentView(R.layout.activity_main)
+        binding =DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.button5.setOnClickListener {
+            val intent= Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/seda/tasarim/SignUp.kt
+++ app/src/main/java/com/seda/tasarim/SignUp.kt
@@ -1,11 +1,22 @@
 package com.seda.tasarim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.seda.tasarim.databinding.ActivityMainBinding
+import com.seda.tasarim.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var  binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+     //   setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+
+        binding.button4.setOnClickListener {
+            val intent= Intent(this,LimitedAccess::class.java)
+            startActivity(intent)
+        }
     }
 }
